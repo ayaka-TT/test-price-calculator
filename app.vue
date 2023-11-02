82fc3eb8ad333b3e9f76e0438fbf7db25964dc0e
Ayaka Tsubouchi <ayaka.tsubouchi@tokyotechies.com>
2023-11-02 18:06:27 +0900

    added input

--- app.vue
+++ app.vue
@@ -1,5 +1,30 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+
+    <div class="py-10">
+  
+      <FormKit
+        ref="formRef"
+        type="form"
+        :actions="false"
+        name="contactFormName"
+        @submit="()=>{}"
+      >
+      <h1>HoHoHo🎅</h1>
+      <p>present request</p>
+      <input type="text" :value="present" :onChange="onChange(value)">
+      <p>{{ present }}</p>
+
+    
+      </FormKit>
+  </div>
   </div>
 </template>
+<script setup>
+const present = ref('')
+const onChange =(value)=>{
+present.value=value
+}
+
+</script>
