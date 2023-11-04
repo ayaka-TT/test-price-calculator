16e9e4b4cee1771f7f1ce3b236bb8faa3ad86762
Ayaka Tsubouchi <ayaka.tsubouchi@tokyotechies.com>
2023-11-04 22:13:49 +0900

    added dependencies

--- app.vue
+++ app.vue
@@ -1,30 +1,14 @@
 <template>
-  <div>
-    <!-- <NuxtWelcome /> -->
 
-    <div class="py-10">
+  <Html class="html">
+      <NuxtLoadingIndicator />
+      <SeoKit />
+      <Body>
+        <NuxtLayout>
+          <NuxtPage />
+        </NuxtLayout>
+      </Body>
+      <BaseGlobal />
+    </Html>
+  </template>
   
-      <FormKit
-        ref="formRef"
-        type="form"
-        :actions="false"
-        name="contactFormName"
-        @submit="()=>{}"
-      >
-      <h1>HoHoHo🎅</h1>
-      <p>present request</p>
-      <input type="text" :value="present" :onChange="onChange(value)">
-      <p>{{ present }}</p>
-
-    
-      </FormKit>
-  </div>
-  </div>
-</template>
-<script setup>
-const present = ref('')
-const onChange =(value)=>{
-present.value=value
-}
-
-</script>
